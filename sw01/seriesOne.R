e0da37c799a38943ba4c83a136e57c5cc078fcaa
I3enito <bensiegenthaler@gmail.com>
2020-10-03 15:38:31 +0200

    exercise 1.1

---
+++ sw01/seriesOne.R
@@ -0,0 +1,30 @@
+# 1.1
+
+x <- c(4, 2, 1, 3, 3, 5, 7)
+
+x[3]
+
+x[c(1, 4)]
+
+length(x)
+
+x+2
+
+x
+
+sum(x + 2)
+
+x <= 3
+
+# this is like a filter function. Filter out elements which are bigger than 3
+x[x <= 3]
+
+sort(x)
+
+order(x)
+
+x[4] <- 8
+
+x
+
+# 1.2

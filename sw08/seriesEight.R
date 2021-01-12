15039d55290484647dc6345c9e442d21d5488144
I3enito <bensiegenthaler@gmail.com>
2021-01-12 15:07:52 +0100

    new series

---
+++ sw08/seriesEight.R
@@ -0,0 +1,9 @@
+1 - pnorm(q = 3.1, mean = 2.2, sd = 0.03) 
+
+# 8.3
+# a
+pnorm(q = 82, mean = 77, sd = 3) - pnorm(q = 72, mean = 77, sd = 3)
+
+# b
+pnorm(q = 82, mean = 77, sd = 15/8) - pnorm(q = 72, mean = 77, sd = 15/8)
+
